6dd4528ece6d8d786253e0b2d306a7d2377dd35d
Sanzhay Ayurov <ayurov2004@gmail.com>
2023-01-22 12:13:35 +0900

    Change №1 and №2

--- №2.py
+++ №2.py
@@ -11,14 +11,13 @@ engine = sq.create_engine(DSN)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-list_of_facts = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Book.publisher).join(Stock).join(Stock.shop).join(Sale).filter(Publisher.last_name == input('Введите фамилию поэта(Пушкин, Грибоедов, Тургенев): ')).all()
-max_item_list = max([max(map(lambda x: str(x), elem_), key=len) for elem_ in list_of_facts], key=len)
-for item in list_of_facts:
-    item_ = list(map(lambda x: str(x), item))
-    for elem in item_:
-        print(elem.ljust(len(max_item_list) + 5, ' '), end='')
-    print()
-
-
+if __name__ == '__main__':
+    list_of_facts = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Book.publisher).join(Stock).join(Stock.shop).join(Sale).filter(Publisher.last_name == input('Введите фамилию поэта(Пушкин, Грибоедов, Тургенев): ')).all()
+    max_item_list = max([max(map(lambda x: str(x), elem_), key=len) for elem_ in list_of_facts], key=len)
+    for item in list_of_facts:
+        item_ = list(map(lambda x: str(x), item))
+        for elem in item_:
+            print(elem.ljust(len(max_item_list) + 5, ' '), end='')
+        print()
 
 session.close()
